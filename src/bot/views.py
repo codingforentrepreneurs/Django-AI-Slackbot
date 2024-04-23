360e3cf9ccd622fa2fc010f93fb23de169628322
CodingForEntrepreneurs <hello@teamcfe.com>
2024-04-23 14:54:27 -0600

    11 - Echo Slack Messages

--- src/bot/views.py
+++ src/bot/views.py
@@ -1,4 +1,5 @@
 import json 
+import requests
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
@@ -6,6 +7,21 @@ from django.views.decorators.http import require_POST
 import helpers
 
 SLACK_BOT_OAUTH_TOKEN = helpers.config('SLACK_BOT_OAUTH_TOKEN', default=None, cast=str)
+
+
+def send_message(message, channel_id=None):
+    url ="https://slack.com/api/chat.postMessage"
+    headers = {
+        "Content-Type": "application/json; charset=utf-8",
+        "Authorization": f"Bearer {SLACK_BOT_OAUTH_TOKEN}",
+        "Accept": "application/json"
+    }
+    data = {
+        "channel": f"{channel_id}",
+        "text": message
+    }
+    return requests.post(url, json=data, headers=headers)
+    
 
 @csrf_exempt
 @require_POST
@@ -16,7 +32,7 @@ def slack_events_endpoint(request):
     except Exception as e:
         pass
     data_type = json_data.get('type')
-    print(data_type, json_data)
+    print(data_type, json_data.keys(), json_data)
     allowed_data_type = [
         "url_verification",
         "event_callback"
@@ -28,4 +44,11 @@ def slack_events_endpoint(request):
         if challenge is None:
             return HttpResponse("Not Allowed", status=400)
         return HttpResponse(challenge, status=200)
+    elif data_type == "event_callback":
+        event = json_data.get('event') or {}
+        msg_text = event.get('text')
+        # user_id = event.get('user')
+        channel_id = event.get('channel')
+        r = send_message(msg_text, channel_id=channel_id)
+        return HttpResponse("Success", status=200)
     return HttpResponse("Success", status=200)
